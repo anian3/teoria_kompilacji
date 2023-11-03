918d37e19082a2a176bbf50390c8b7a70333fdd1
Ania Nowacka <anna.nowacka146@gmail.com>
2023-11-03 13:58:09 +0100

    Matrices

--- Mparser.py
+++ Mparser.py
@@ -58,13 +58,13 @@ def p_expression_binop_matrix(p):
                    | expression DOTMUL expression
                    | expression DOTDIV expression """
     if p[2] == '.+':
-        p[0] = p[1] + p[3]
+        p[0] = matrix_addition(p[1], p[3])
     elif p[2] == '.-':
-        p[0] = p[1] - p[3]
+        p[0] = matrix_substraction(p[1], p[3])
     elif p[2] == '.*':
-        p[0] = p[1] * p[3]
+        p[0] = matrix_multiplication(p[1], p[3])
     elif p[2] == './':
-        p[0] = p[1] / p[3]
+        p[0] = matrix_division(p[1], p[3])
 
 
 def p_expression_assign(p):
@@ -80,6 +80,35 @@ def p_expression_assign(p):
         p[1] *= p[3]
     elif p[2] == '/=':
         p[1] /= p[3]
+
+
+def p_expression_transpose(p):
+    """ expression: "'" expression """
+    p[0] = matrix_transpose(p[1])
+
+
+def p_matrix_initialization(p):
+    """ expression : '[' matrix_rows ']' """
+    p[0] = p[2]
+
+
+def p_matrix_rows(p):
+    """matrix_rows :  matrix_row_values  """
+    p[0] = [p[1]]
+
+
+def p_matrix_rows_multiple(p):
+    """matrix_rows : matrix_rows ',' matrix_row_values """
+    p[0] = p[1] + [p[3]]
+
+
+def p_matrix_row_values(p):
+    """matrix_row_values : matrix_row_values ',' expression
+                            | matrix_row_values """
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[3]]
 
 
 def p_expression_group(t):
@@ -114,7 +143,6 @@ def p_expression_string(t):
     t[0] = t[1]
 
 
-
 def p_error(p):
     if p:
         print("Syntax error at line {0}: LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
@@ -125,6 +153,53 @@ def p_error(p):
 # def p_program(p):
 #     """program : instructions_opt"""
 
+# funkcje pomocnicze
+
+def matrix_addition(A, B):
+    if isinstance(A, list) and isinstance(B, list):
+        if len(A) != len(B) or len(A[0]) != len(B[0]):
+            raise ValueError("Dodawanie element po elemencie na różnych rozmiarach macierzy.")
+        return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    else:
+        raise ValueError("Dodawanie macierzowe wykonywane nie na macierzach.")
+
+
+def matrix_substraction(A, B):
+    if isinstance(A, list) and isinstance(B, list):
+        if len(A) != len(B) or len(A[0]) != len(B[0]):
+            raise ValueError("Odejmowanie element po elemencie na różnych rozmiarach macierzy.")
+        return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    else:
+        raise ValueError("Odejmowanie macierzowe wykonywane nie na macierzach.")
+
+
+def matrix_multiplication(A, B):
+    if isinstance(A, list) and isinstance(B, list):
+        if len(A) != len(B) or len(A[0]) != len(B[0]):
+            raise ValueError("Mnożenie element po elemencie na różnych rozmiarach macierzy.")
+        return [[A[i][j] * B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    else:
+        raise ValueError("Mnożenie macierzowe wykonywane nie na macierzach.")
+
+
+def matrix_division(A, B):
+    if isinstance(A, list) and isinstance(B, list):
+        if len(A) != len(B) or len(A[0]) != len(B[0]):
+            raise ValueError("Dzielenie element po elemencie na różnych rozmiarach macierzy.")
+        for i in range(len(B)):
+            for j in range(len(B[0])):
+                if B[i][j] == 0:
+                    raise ValueError("Dzielenie przez zero.")
+        return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    else:
+        raise ValueError("Odejmowanie macierzowe wykonywane nie na macierzach.")
+
+
+def matrix_transpose(A):
+    if isinstance(A, list):
+        return [[row[i] for row in A] for i in range(len(A[0]))]
+    else:
+        raise ValueError("Transpozycja wykonywana nie dla macierzy.")
 
 
 parser = yacc.yacc()
