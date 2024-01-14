1d9b213e29b101efbb30ba7e4bfbf34b83aa570e
Ania Nowacka <anna.nowacka146@gmail.com>
2024-01-14 23:52:20 +0100

    main

--- Interpreter/main.py
+++ Interpreter/main.py
@@ -10,7 +10,7 @@ from Interpreter import Interpreter
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "fibonacci.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
@@ -25,7 +25,8 @@ if __name__ == '__main__':
     typeChecker = TypeChecker()
     typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
 
-    ast.accept(Interpreter())
+    if typeChecker.all_correct:
+        ast.accept(Interpreter())
     # in future
     # ast.accept(OptimizationPass1())
     # ast.accept(OptimizationPass2())
